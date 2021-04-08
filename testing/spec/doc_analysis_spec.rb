f957b731824b484ddefd7aad09099b654a6e2032
John Arboleda <JohnJack0126@gmail.com>
2021-04-08 15:45:01 -0500

    Fix Rubocop test array LineLenght lint

--- testing/spec/doc_analysis_spec.rb
+++ testing/spec/doc_analysis_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../lib/parsed_doc.rb'
+require_relative '../../lib/parsed_doc'
 require_relative '../../lib/doc_analysis'
 require 'strscan'
 
@@ -9,13 +9,13 @@ describe DocAnalysis do
     it 'Returns a colorized array of mandatory html tags missing' do
       file_path = 'negative_example.html'
       parsed_array = ParsedDoc.new(file_path).parsed_array
-      expect(basic_analysis(parsed_array)).to eql([
-        "\e[0;31;49mError: \e[0mDOCTYPE tag not found", 
-        "\e[0;31;49mError: \e[0mhtml tag not found", 
-        "\e[0;31;49mError: \e[0mhead tag not found", 
-        "\e[0;31;49mError: \e[0mbody tag not found"
-        ]
-      )
+      expect(basic_analysis(parsed_array))
+        .to eql([
+                  "\e[0;31;49mError: \e[0mDOCTYPE tag not found",
+                  "\e[0;31;49mError: \e[0mhtml tag not found",
+                  "\e[0;31;49mError: \e[0mhead tag not found",
+                  "\e[0;31;49mError: \e[0mbody tag not found"
+                ])
     end
     it 'Returns nothing if mandatory html tags were include in the document' do
       file_path = 'positive_example.html'
--- testing/spec/tags_analysis_spec.rb
+++ testing/spec/tags_analysis_spec.rb
@@ -1,5 +1,5 @@
-require_relative '../../lib/parsed_doc.rb'
-require_relative '../../lib/tags_analysis.rb'
+require_relative '../../lib/parsed_doc'
+require_relative '../../lib/tags_analysis'
 require 'strscan'
 
 describe TagsAnalysis do
@@ -9,15 +9,15 @@ describe TagsAnalysis do
     it 'Returns a colorized array of errors from tags and attributes' do
       file_path = 'negative_example.html'
       parsed_array = ParsedDoc.new(file_path).parsed_array
-      expect(tags_analysis(parsed_array)).to eql([
-        "\e[0;34;49mSuggestion: \e[0mline 13 col 69: avoid whitespace after attribute",
-        "\e[0;34;49mSuggestion: \e[0m line 13 col 70: avoid whitespace after attribute",
-        "\e[0;33;49mWarning: \e[0mline 17 col 3: tag H1 is not lowercase",
-        "\e[0;31;49mError: \e[0mline 19 col 3: invalid tag artic",
-        "\e[0;33;49mWarning: \e[0mline 23 col 37: attribute Href is not lowercase",
-        "\e[0;34;49mSuggestion: \e[0m line 23 col 41: avoid whitespace after attribute",
-        ]
-      )
+      expect(tags_analysis(parsed_array))
+        .to eql([
+                  "\e[0;34;49mSuggestion: \e[0mline 13 col 69: avoid whitespace after attribute",
+                  "\e[0;34;49mSuggestion: \e[0m line 13 col 70: avoid whitespace after attribute",
+                  "\e[0;33;49mWarning: \e[0mline 17 col 3: tag H1 is not lowercase",
+                  "\e[0;31;49mError: \e[0mline 19 col 3: invalid tag artic",
+                  "\e[0;33;49mWarning: \e[0mline 23 col 37: attribute Href is not lowercase",
+                  "\e[0;34;49mSuggestion: \e[0m line 23 col 41: avoid whitespace after attribute"
+                ])
     end
     it 'Returns nothing if tags and attributes are formated properly' do
       file_path = 'positive_example.html'
